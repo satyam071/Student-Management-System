f9e51351464b901f0e7874f6fa86100f20037848
unknown <ysatyam0071@gmail.com>
2026-05-19 14:53:22 +0530

    Setup Routing

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router";
 
 interface Props {
 
@@ -8,6 +9,7 @@ const Login: React.FC<Props> = (props) =>{
   return (
     <div>
         This is a Login Section
+        Don't have an account <Link  to={"/signup"}><span className="text-blue-600">Click here</span></Link>
     </div>
   );
 };
--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,16 +1,19 @@
 import React from "react";
+import { Link } from "react-router";
 
 interface Props {
 
 }
 
-const Signup: React.FC<Props> = (props) =>{
-  return (
-    <div>
-        This is a signup section
-        
-    </div>
-  );
+const Signup: React.FC<Props> = (props) => {
+    return (
+        <div>
+            This is a signup section
+            Alredy have an account <Link to={"/login"}><span className="text-blue-600">Click here</span></Link>
+
+
+        </div>
+    );
 };
 
 export default Signup;
